137e747814405132d15bcb5f9d1bb19c7789078b
abid313 <abidfajar50@gmail.com>
2024-03-14 00:46:55 +0700

    Add resource field and create post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse, abort, fields, marshal_with
+from flask_restful import Api, Resource, abort, reqparse, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -16,7 +16,7 @@ class VideoModel(db.Model):
     def __repr__(self):
         return f"Video(name={self.name}, views={self.views}, likes={self.likes})"
 
-## Menggunakan app context
+## Menggunakan app context untuk first initiate db
 # with app.app_context():
 #     db.create_all()
 
@@ -25,24 +25,50 @@ video_put_args.add_argument("name", type=str, help="Name of the video is require
 video_put_args.add_argument("views", type=int, help="Views of the video is required", required=True)
 video_put_args.add_argument("likes", type=int, help="Likes of the video is required", required=True)
 
+resource_fields = {
+    'id': fields.Integer,
+    'name': fields.String,
+    'views': fields.Integer,
+    'likes': fields.Integer
+}
 
 class Video(Resource):
+    @marshal_with(resource_fields)
     def get(self, video_id):
-        abort_if_video_id_doesnt_exit(video_id)
-        return videos[video_id]
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if not result:
+            abort(404, message="Could not find video...")
+        return result
 
+    @marshal_with(resource_fields)
     def put(self, video_id):
-        abort_if_video_id_exist(video_id)
         args = video_put_args.parse_args()
-        videos[video_id] = args
-        return videos[video_id], 201
-    
+        result = VideoModel.query.filter_by(id=video_id).first()
+        if result:
+            abort(409, message="Video ID taken...")
+        video = VideoModel(id=video_id, name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
+
     def delete(self, video_id):
-        abort_if_video_id_doesnt_exit(video_id)
-        del videos[video_id]
+        video = VideoModel.query.filter_by(id=video_id).first()
+        if not video:
+            abort(404, message="Video does not exist...")
+        db.session.delete(video)
+        db.session.commit()
         return '', 204
 
+    @marshal_with(resource_fields)
+    def post(self):
+        args = video_put_args.parse_args()
+        video = VideoModel(name=args['name'], views=args['views'], likes=args['likes'])
+        db.session.add(video)
+        db.session.commit()
+        return video, 201
+
 api.add_resource(Video, "/video/<int:video_id>")
+api.add_resource(Video, "/video")
 
 if __name__ == "__main__":
     app.run(debug=True)
